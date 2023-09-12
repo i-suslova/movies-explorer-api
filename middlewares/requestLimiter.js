72924b6363c72bc5aec6fcb3b1b1592433c75895
Irina <i.sus2011@yandex.ru>
2023-09-12 19:51:55 +0300

    Авторизация несуществующего пути

--- middlewares/requestLimiter.js
+++ middlewares/requestLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const { ERRORS } = require('../utils/constants/errorMessages');
 
-// ограничения запросов (100 запросов в час)
+// ограничения запросов (100 запросов в час с одного IP адреса)
 module.exports.limiter = rateLimit({
   windowMs: 60 * 60 * 100,
   max: 100,
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const errorHandler = require('../middlewares/errorHandler');
-const { NotFoundError, RateLimitError } = require('../utils/errors/indexErrors');
+const { NotFoundError } = require('../utils/errors/indexErrors');
 const { ERRORS } = require('../utils/constants/errorMessages');
+const { authUser } = require('../middlewares/auth');
 
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
@@ -12,16 +12,8 @@ router.use('/', authRouter);
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
 
-router.use((err, req, res, next) => {
-  if (err instanceof RateLimitError) {
-    errorHandler(err, req, res, next);
-  } else {
-    next(err);
-  }
-});
-
 // несуществующий путь
-router.use('/*', (req, res, next) => {
+router.use('/*', authUser, (req, res, next) => {
   next(new NotFoundError(ERRORS.PAGE_NOT_FOUND));
 });
 
